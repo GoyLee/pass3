8ca2c39232f585c40f8e0c669bc75d3f7db0216b
stanleymao <31478981+stanleymao@users.noreply.github.com>
2018-02-06 17:01:33 +0800

    2-5

--- app/model/requirement.js
+++ app/model/requirement.js
@@ -4,18 +4,18 @@ module.exports = app => {
     var RequirementSchema = new mongoose.Schema({
       code: { type: String  }, //required: true, , unique: true //工号
       name: { type: String , required: true }, //名称
-      desc: { type: String , required: true }, //描述
-      necessity: { type: String , required: true }, //必要性说明
+      //desc: { type: String , required: false }, //描述
+      project: { type: String , required: false }, //IT项目 type: mongoose.Schema.Types.ObjectId
       class: { type: String  }, //分类：项目群，项目，年度，阶段，tags?
       type: { type: String  }, //应用，设备，网络，服务
       status: { type: String  }, //状态：提出，处理中（转项目），取消/拒绝，挂起，关闭
-      provider: { type: mongoose.Schema.Types.ObjectId }, //提出人，指向party
+      demander: { type: mongoose.Schema.Types.ObjectId }, //提出人，指向party
       department: { type: mongoose.Schema.Types.ObjectId }, //提出人所属部门，指向party
       owner: { type: mongoose.Schema.Types.ObjectId }, //责任人，指向party
       ownerdepartment: { type: mongoose.Schema.Types.ObjectId }, //责任人所属部门，指向party
-      budget: {type: Number }, //预算额、总额，初始设定的
-      amount: {type: Number }, //根据下级需求汇总上来的预算总额
-      fundsource: { type: String }, //资金来源：ARJ21，C919，CR929，课题，自筹
+      //budget: {type: Number }, //预算额、总额，初始设定的
+      //amount: {type: Number }, //根据下级需求汇总上来的预算总额
+      //fundsource: { type: String }, //资金来源：ARJ21，C919，CR929，课题，自筹
       createdAt: { type:Date, default: Date.now()}, //
       updatedAt: { type:Date, default: Date.now() },
       pid: { type: mongoose.Schema.Types.ObjectId, ref: 'Requirement' }, //上级需求，唯一
